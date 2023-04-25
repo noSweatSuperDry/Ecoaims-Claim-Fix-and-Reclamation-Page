840874e3c0ae24522990a1e85e793482bdbd64c6
Zahid Abdullah <zahid.abdullah@ecoaims.com>
2023-04-25 09:56:32 +0300

    Basics

--- client/src/components/Claim/index.js
+++ client/src/components/Claim/index.js
@@ -25,58 +25,57 @@ function Claim() {
             <p>Please fill up the forms carefully. In case you made mistakes, please edit or delete and re-entry</p>
 
             <div className="textAndButton">
-                <label>Product Name: </label>
+                <label>1. Product Name: </label>
                 <input type="text" name="productName"
-                    onChange={handleInputChange} /><br />
-                <label>Device Serial Number: </label>
+                    onChange={handleInputChange} required /><br />
+                <label>2. Device Serial Number: </label>
                 <input type="number" name="productSerialNumber"
-                    onChange={handleInputChange} /><br />
-                <label>Issue Date: </label>
+                    onChange={handleInputChange} required/><br />
+                <label>3. Issue Date: </label>
                 <input type="Date" name="issueDate"
-                    onChange={handleInputChange} /><br />
-                <label>Repair Date: </label>
+                    onChange={handleInputChange} required/><br />
+                <label>4. Repair Date: </label>
                 <input type="Date" name="repairDate"
-                    onChange={handleInputChange} /><br />
-                <label>Device Manufacturing Date: </label>
+                    onChange={handleInputChange} required/><br />
+                <label>5. Device Manufacturing Date: </label>
                 <input type="Date" name="manufacturingDate"
                     onChange={handleInputChange} /><br />
-                <label>PCB Model Number: </label>
+                <label>6. PCB Model Number: </label>
                 <input type="text" name="pcbModelNo"
                     onChange={handleInputChange} /><br />
-                <label>Laser Serial Number: </label>
+                <label>7. Laser Serial Number: </label>
                 <input type="text" name="laserSerialNumber"
                     onChange={handleInputChange} /><br />
-                <label>LemonSoft Issue Number: </label>
+                <label>8. LemonSoft Issue Number: </label>
                 <input type="number" name="lemonSoftIssueNumber"
-                    onChange={handleInputChange} /><br />
-                <label>Country: </label>
+                    onChange={handleInputChange} required/><br />
+                <label>9. Country: </label>
                 <input type="text" name="country"
-                    onChange={handleInputChange} /><br />
-                <label>Report By Customer: </label>
+                    onChange={handleInputChange} required/><br />
+                <label>10. Report By Customer: </label>
                 <input type="text" name="reportByCustomer"
                     onChange={handleInputChange} /><br />
-                <label>Report By Eko-aims: </label>
+                <label>11. Report By Eko-aims: </label>
                 <input type="text" name="reportByEcoaims"
-                    onChange={handleInputChange} /><br />
-                <label>Cause is known ? </label>
+                    onChange={handleInputChange} required/><br />
+                <label>12. Cause is known ? </label>
                 <input type="text" name="causeKnown"
                     onChange={handleInputChange} /><br />
-                <label>What is the cause ? </label>
+                <label>13. What is the cause ? </label>
                 <input type="text" name="whatIsTheCause"
                     onChange={handleInputChange} /><br />
-                <label>Conclusion: </label>
+                <label>14. Conclusion: </label>
                 <input type="text" name="Conclusion"
                     onChange={handleInputChange} /><br />
-
-                <label>What message sent to customer? </label>
+                <label>15. What message sent to customer? </label>
                 <input type="text" name="whatMsgToCustomer"
                     onChange={handleInputChange} /><br />
-                <label>Repaired/Changed components: </label>
+                <label>16. Repaired/Changed components: </label>
                 <input type="text" name="componentsUsedInRepair"
-                    onChange={handleInputChange} /><br />
-                <label>Please Write your Name: </label>
+                    onChange={handleInputChange} required/><br />
+                <label>17. Please Write your Name: </label>
                 <input type="text" name="userName"
-                    onChange={handleInputChange} /><br />
+                    onChange={handleInputChange} required/><br />
                 <br />
                 <button className="idPassCard" type="submit" onClick={handleSubmit}>Submit</button>
             </div>
--- client/src/index.js
+++ client/src/index.js
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-    <Banner/>
-    <Home/>
-    <Footer/>
+      <Banner />
+      <Home />
+      <Footer />
     </div>
   </React.StrictMode>
 );
