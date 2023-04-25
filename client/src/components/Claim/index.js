f6bef5e2a134636de07f4b1c05b77457bdbca638
Zahid Abdullah <zahid.abdullah@ecoaims.com>
2023-04-25 09:35:17 +0300

    Update index.js

--- client/src/components/Claim/index.js
+++ client/src/components/Claim/index.js
@@ -1,61 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 
 
-function Claim(){
-return(
-<div className="pageOutlet">
-<h2>Report Claims and Fixes Here.</h2>
-<p>Please fill up the forms carefully. In case you made mistakes, please edit or delete and re-entry</p>
+function Claim() {
+    const [productInfo, setProductInfo] = useState({});
 
-<div className="textAndButton">
-<label>Issue Date: </label>
-<input type="Date"/><br/>
-<label>Repair Date: </label>
-<input type="Date"/><br/>
-<label>Device Manufacturing Date: </label>
-<input type="Date"/><br/>
-<label>Product Name: </label>
-<input type="text"/><br/>
-<label>Device Serial Number: </label>
-<input type="number"/><br/>
-<label>PCB Model Number: </label>
-<input type="text"/><br/>
-<label>Laser Serial Number: </label>
-<input type="text"/><br/>
-<label>LemonSoft Issue Number: </label>
-<input type="number"/><br/>
-<label>Country: </label>
-<input type="text"/><br/>
-<label>Report By Customer: </label>
-<input type="text"/><br/>
-<label>Report By Eko-aims: </label>
-<input type="text"/><br/>
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setProductInfo((prevProductInfo) => ({
+            ...prevProductInfo,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(productInfo);
+    };
 
 
-<label>Cause is known ? </label>
-<select> 
-<option>Yes</option>
-<option>No</option>
-</select><br/>
-<label>What is the cause ? </label>
-<input type="text"/><br/>
-<label>Conclusion: </label>
-<input type="text"/><br/>
 
-<label>What message sent to customer? </label>
-<input type="text"/><br/>
-<label>Repaired/Changed components: </label>
-<input type="text"/><br/>
-<label>Please Write your Name: </label>
-<input type="text"/><br/>
-<br/>
-<button className="idPassCard"
-//type="submit"
-//onClick={{}=}
-> Submit</button>
-</div>
-</div>
-);
+    return (
+        <div className="pageOutlet">
+            <h2>Report Claims and Fixes Here.</h2>
+            <p>Please fill up the forms carefully. In case you made mistakes, please edit or delete and re-entry</p>
+
+            <div className="textAndButton">
+                <label>Product Name: </label>
+                <input type="text" name="productName"
+                    onChange={handleInputChange} /><br />
+                <label>Device Serial Number: </label>
+                <input type="number" name="productSerialNumber"
+                    onChange={handleInputChange} /><br />
+                <label>Issue Date: </label>
+                <input type="Date" name="issueDate"
+                    onChange={handleInputChange} /><br />
+                <label>Repair Date: </label>
+                <input type="Date" name="repairDate"
+                    onChange={handleInputChange} /><br />
+                <label>Device Manufacturing Date: </label>
+                <input type="Date" name="manufacturingDate"
+                    onChange={handleInputChange} /><br />
+                <label>PCB Model Number: </label>
+                <input type="text" name="pcbModelNo"
+                    onChange={handleInputChange} /><br />
+                <label>Laser Serial Number: </label>
+                <input type="text" name="laserSerialNumber"
+                    onChange={handleInputChange} /><br />
+                <label>LemonSoft Issue Number: </label>
+                <input type="number" name="lemonSoftIssueNumber"
+                    onChange={handleInputChange} /><br />
+                <label>Country: </label>
+                <input type="text" name="country"
+                    onChange={handleInputChange} /><br />
+                <label>Report By Customer: </label>
+                <input type="text" name="reportByCustomer"
+                    onChange={handleInputChange} /><br />
+                <label>Report By Eko-aims: </label>
+                <input type="text" name="reportByEcoaims"
+                    onChange={handleInputChange} /><br />
+                <label>Cause is known ? </label>
+                <input type="text" name="causeKnown"
+                    onChange={handleInputChange} /><br />
+                <label>What is the cause ? </label>
+                <input type="text" name="whatIsTheCause"
+                    onChange={handleInputChange} /><br />
+                <label>Conclusion: </label>
+                <input type="text" name="Conclusion"
+                    onChange={handleInputChange} /><br />
+
+                <label>What message sent to customer? </label>
+                <input type="text" name="whatMsgToCustomer"
+                    onChange={handleInputChange} /><br />
+                <label>Repaired/Changed components: </label>
+                <input type="text" name="componentsUsedInRepair"
+                    onChange={handleInputChange} /><br />
+                <label>Please Write your Name: </label>
+                <input type="text" name="userName"
+                    onChange={handleInputChange} /><br />
+                <br />
+                <button className="idPassCard" type="submit" onClick={handleSubmit}>Submit</button>
+            </div>
+        </div>
+    );
+
 }
 
 export default Claim;
