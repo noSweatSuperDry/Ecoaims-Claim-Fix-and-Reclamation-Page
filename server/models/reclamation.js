0b65b5d610a090c03414b5e900d9dffea3eb2b73
Zahid Abdullah <zaevanphy@gmail.com>
2023-04-26 23:07:17 +0300

    Update reclamation.js

---
+++ server/models/reclamation.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const reclamationSchema = new mongoose.Schema({
+  issueDate: {
+    type: String,
+    required: true,
+  },
+  partName: {
+    type: String,
+    required: true,
+  },
+  deviceSerialNumber: {
+    type: String,
+    required: true,
+  },
+
+  howMany: {
+    type: String,
+    required: true,
+  },
+  lemonSoftIssueNumber: {
+    type: String,
+    required: true,
+  },
+  electricalComponentType: {
+    type: String,
+    required: false,
+  },
+  mechanicalComponentType: {
+    type: String,
+    required: false,
+  },
+  information: {
+    type: String,
+    required: false,
+  },
+
+  causeKnownR: {
+    type: String,
+    required: true,
+  },
+  whatIsTheCauseR: {
+    type: String,
+    required: false,
+  },
+  conclusionR: {
+    type: String,
+    required: true,
+  },
+  repairedChangedComponent: {
+    type: String,
+    required: false,
+  },
+
+  userNameR: {
+    type: String,
+    required: true,
+  },
+});
+
+const reclamation = mongoose.model("reclamation", reclamationSchema);
+module.exports = reclamation;
