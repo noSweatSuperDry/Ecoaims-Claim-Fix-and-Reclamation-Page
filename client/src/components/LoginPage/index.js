56aac48159353afab8aab068a378325a29874efd
Zahid Abdullah <zahid.abdullah@ecoaims.com>
2023-04-27 22:24:28 +0300

    Update index.js

--- client/src/components/LoginPage/index.js
+++ client/src/components/LoginPage/index.js
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
+import Axios from "axios";
 
-function LoginPage({ Login, error }) {
+
+function LoginPage() {
   const [details, setDetails] = useState({ userId: "", password: "" });
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    Login(details);
+  console.log(details);
+  
+  const submitHandler = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+  
+    const userId = details.userId;
+    const password = details.password;
+  
+    Axios.get(`http://localhost:5001/users/${userId}/${password}`) // Send a GET request to the API with the user details
+      .then((response) => {
+        // Handle successful response
+        console.log(response.data);
+      })
+      .catch((error) => {
+        // Handle error
+        console.error(error);
+      });
   };
+
   return (
     <form onSubmit={submitHandler}>
       <div className="loginForm">
@@ -18,7 +34,7 @@ function LoginPage({ Login, error }) {
             id="userId"
             autoFocus
             onChange={(e) => setDetails({ ...details, userId: e.target.value })}
-            value={details.userId}
+            
           />
         </label>
         <label className="idPassCard" htmlFor="password">
@@ -29,7 +45,7 @@ function LoginPage({ Login, error }) {
             onChange={(e) =>
               setDetails({ ...details, password: e.target.value })
             }
-            value={details.password}
+            autoFocus
           />
         </label>
         <input
