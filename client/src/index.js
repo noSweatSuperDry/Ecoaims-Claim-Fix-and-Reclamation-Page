8a3bf7aa3e2817ffb70cdc4e470c2d771a82c743
Zahid Abdullah <zaevanphy@gmail.com>
2023-05-02 08:14:49 +0300

    Update

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./css/App.css";
-import Banner from './components/Banner/index';
-import LoginPage from './components/LoginPage/index';
-import Footer from './components/Footer'
+import Banner from "./components/Banner/index";
+import LoginPage from "./components/LoginPage/index";
+import Footer from "./components/Footer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>
@@ -15,4 +15,3 @@ root.render(
     </div>
   </React.StrictMode>
 );
-
