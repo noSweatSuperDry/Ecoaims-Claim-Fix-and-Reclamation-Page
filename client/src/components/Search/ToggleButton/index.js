a05a268f109136bec4c1b3899d9ac3e44c20d75d
Zahid Abdullah <zahid.abdullah@ecoaims.com>
2023-05-03 14:55:27 +0300

    Update index.js

--- client/src/components/Search/ToggleButton/index.js
+++ client/src/components/Search/ToggleButton/index.js
@@ -3,16 +3,24 @@ import SearchBox from '../SearchBox';
 import ListItem from '../ListItem';
 function ToggleButton({productList}) {
     const [isToggled, setIsToggled] = useState(true);
-
+    const [isToggledRec, setIsToggledRec] = useState(true);
     const handleToggle = () => {
       setIsToggled(!isToggled);
+   
+    }
+    const handleToggleReclamation = () => {
+      setIsToggledRec(!isToggledRec);
+      setIsToggled(true);
       console.log(isToggled);
     }
-  
+
     return (
       <div>
         <button onClick={handleToggle}  className="idPassCard">
-          {isToggled ? 'Show All Products' : 'Search DataBase'}
+          {isToggled ? 'Show All Claims' : 'Search Claims DataBase by Lemonsoft ID'}
+        </button>
+        <button onClick={handleToggleReclamation}  className="idPassCard">
+          {isToggledRec ? 'Show All Reclamations' : 'Search Reclamation DataBase'}
         </button>
         {!isToggled ?(<ListItem productList={productList} />):(<SearchBox/>)}
       </div>
