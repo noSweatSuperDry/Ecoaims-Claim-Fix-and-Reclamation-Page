b16f9479267cf63064cc32f7dc6ae5e92c5f90ba
Zahid Abdullah <zahid.abdullah@ecoaims.com>
2023-05-03 14:15:06 +0300

    update

--- client/README.md
+++ client/README.md
@@ -68,3 +68,11 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+## VERSION HISTORY
+
+## ISSUES OPEN
+
+1. Reclamation not working.
+2.Edit, Update and Delete not working
--- client/src/components/LogOut/index.js
+++ client/src/components/LogOut/index.js
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
-function LogOut() {
-  const handleSubmit = () => {};
+function Modal(props) {
+  const handleConfirm = () => {
+    props.onConfirm();
+    props.onClose();
+  };
 
   return (
     <div>
-      <button className="idPassCard" onClick={handleSubmit}>
+      <div className="loginForm">
+        <h1>{props.title}</h1>
+        <h2>{props.message}</h2>
+        <div >
+          <button className="idPassCard" onClick={props.onClose}>Cancel</button>
+          <button className="idPassCard" onClick={handleConfirm}>OK</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function LogOut() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = () => {
+    sessionStorage.removeItem("token");
+    window.location.reload();
+  };
+
+  const handleConfirmLogout = () => {
+    handleSubmit();
+    setShowModal(false);
+  };
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div>
+      <button className="idPassCard" onClick={handleShowModal}>
         Log Out
       </button>
+      {showModal && (
+        <Modal
+          title="Log out"
+          message="Are you sure you want to log out?"
+          onClose={handleCloseModal}
+          onConfirm={handleConfirmLogout}
+        />
+      )}
     </div>
   );
 }
